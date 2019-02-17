d8501ea00ba4c62e3d84f9580a6325027c18727d
tBlueMage <47580410+tBlueMage@users.noreply.github.com>
2019-02-17 18:51:33 -0500

    Core Stuff Added

---
+++ UI Assembling/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    public GameObject prefabChunk;
+    public GameObject player;
+    public Transform playerTransform;
+    public Text scoreText;
+
+
+    List<GameObject> chunks = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(chunks.Count > 0)
+        {
+            if(playerTransform.position.z-chunks[0].transform.position.z > 60)
+            {
+                Destroy(chunks[0]);
+                chunks.RemoveAt(0);
+            }
+        }
+
+        while(chunks.Count < 5)
+        {
+            Vector3 position = Vector3.zero;
+
+            if(chunks.Count > 0)
+            {
+                position = chunks[chunks.Count - 1].transform.Find("Connector").position;
+            }
+
+            GameObject obj = Instantiate(prefabChunk, position, Quaternion.identity);
+            chunks.Add(obj);
+        }
+
+
+    }
+
+    void setScoreText()
+    {
+        int score = player.GetComponent<PlayerScript>().score;
+        scoreText.text = "Score:" + score;
+    }
+
+}
---
+++ UI Assembling/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public int speed;
+
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0, 0, 1);
+
+        transform.position += (movement * speed);
+        //rb.AddForce(movement * speed);
+
+        score += speed;
+    }
+}
