aa554ac9e9bdc430a6ada784996d073b427c7b2f
tBlueMage <47580410+tBlueMage@users.noreply.github.com>
2019-02-28 18:43:46 -0500

    022819

--- EndlessRunner/New Unity Project/Assets/Shane/script/FartMeterScript.cs
+++ EndlessRunner/New Unity Project/Assets/Shane/script/FartMeterScript.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FartMeterScript : MonoBehaviour
 {
     public PlayerRun player;
+    public Slider slider; 
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,6 @@ public class FartMeterScript : MonoBehaviour
     {
         float currSize = player.fart;
 
-        Vector3 size = new Vector3(currSize, 1, 1);
-
-        transform.localScale = size;
-        // 574, 268, 0 is the middle of the screen
-        Vector3 movement = new Vector3(1009, 488, 0);
-
-        transform.position = movement;
+        slider.value = currSize;
     }
 }
--- EndlessRunner/New Unity Project/Assets/Shane/script/PointerScript.cs
+++ EndlessRunner/New Unity Project/Assets/Shane/script/PointerScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointerScript : MonoBehaviour
 {
 
     public PlayerRun player;
+    public Slider slider;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,9 @@ public class PointerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int finalFat = 30 + (PlayerRun.fat / 25);
+        float fatNorm = (PlayerRun.fat - (-900f)) / 2400f;
+        //float finalFat = 30f + (PlayerRun.fat / 25);
         // 574, 268, 0 is the middle of the screen
-        Vector3 movement = new Vector3(550 + finalFat, 485, 0);
-
-        transform.position = (movement);
+        slider.value = Mathf.Clamp(fatNorm, 0 ,1);
     }
 }
