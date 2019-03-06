5d159681af88ca51151ea0d14226cfa01353079e
kusmies <kusmies@ferris.edu>
2019-03-06 17:59:23 -0600

    all sections done

--- EndlessRunner-Current/New Unity Project/Assets/Scripts/Chunk.cs
+++ EndlessRunner-Current/New Unity Project/Assets/Scripts/Chunk.cs
@@ -15,6 +15,8 @@ public class Chunk : MonoBehaviour
     public GameObject steelWall;
     public GameObject itemspawn;
 
+    public int difficulty;
+
     private int lane;
     private float wallOffset = 0;
     private int objectProbability;
@@ -23,36 +25,72 @@ public class Chunk : MonoBehaviour
 
     void Start()
     {
+        calcDifficulty();
 
+        if (difficulty == 1) {
+            lane = Random.Range(1, 4);
+            if (lane == 1)
+            {
+                D1SpawnObjectAt("spawn1");
+            }
 
+            else if (lane == 2)
+            {
+                D1SpawnObjectAt("spawn2");
+            }
 
-        lane = Random.Range(1, 4);
-        if (lane == 1)
-        {
-            SpawnObjectAt("spawn1");
+            else if (lane == 3)
+            {
+                D1SpawnObjectAt("spawn3");
+            }
         }
-
-        else if (lane == 2)
+        if (difficulty == 2)
         {
-            SpawnObjectAt("spawn2");
+            lane = Random.Range(1, 4);
+            if (lane == 1)
+            {
+                D2SpawnObjectAt("spawn1");
+            }
+
+            else if (lane == 3)
+            {
+                D2SpawnObjectAt("spawn2");
+            }
+
+            else if (lane == 3)
+            {
+                D2SpawnObjectAt("spawn3");
+            }
         }
-
-        else if (lane == 3)
+        if (difficulty == 3)
         {
-            SpawnObjectAt("spawn3");
+            lane = Random.Range(1, 4);
+            if (lane == 1)
+            {
+                D3SpawnObjectAt("spawn1");
+            }
+
+            else if (lane == 2)
+            {
+                D3SpawnObjectAt("spawn2");
+            }
+
+            else if (lane == 3)
+            {
+                D3SpawnObjectAt("spawn3");
+            }
         }
 
         SpawnStreetLightAt("streetLight RM");
         SpawnStreetLightAt("streetLight LM");
         SpawnBuildingAt("Building L");
         SpawnBuildingAt("Building R");
-
     }
 
 
-    void SpawnObjectAt(string name)
+    void D1SpawnObjectAt(string name)
     {
-  objectProbability = Random.Range(0, 100);
+            objectProbability = Random.Range(0, 100);
 
 
       
@@ -94,10 +132,105 @@ public class Chunk : MonoBehaviour
                 position.y += .25f;
                 Instantiate(apple, position, Quaternion.identity);
             }
+    }
 
+    void D2SpawnObjectAt(string name)
+    {
+        objectProbability = Random.Range(0, 100);
+
+
+
+        if (objectProbability <= 10)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(itemspawn, position, Quaternion.identity);
+        }
+        
+        if (objectProbability > 10 && objectProbability <= 25)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(brickWall, position, Quaternion.identity);
+        }
+        if (objectProbability > 25 && objectProbability <= 35)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(steelWall, position, Quaternion.identity);
+        }
+        if (objectProbability > 35 && objectProbability <= 45)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(garbageBags, position, Quaternion.identity);
+        }
+        if (objectProbability >45 && objectProbability <= 76)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(iceCream, position, Quaternion.identity);
+        }
+        if (objectProbability > 76 && objectProbability <= 79)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(musclePowder, position, Quaternion.identity);
+        }
+        if (objectProbability > 79 && objectProbability <= 100)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(apple, position, Quaternion.identity);
+        }
+        
+        
+    }
+
+
+    void D3SpawnObjectAt(string name)
+    {
+        objectProbability = Random.Range(0, 100);
+
+
+
+        if (objectProbability <= 20)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(itemspawn, position, Quaternion.identity);
+        }
+        
+        if (objectProbability > 20 && objectProbability <= 30)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(brickWall, position, Quaternion.identity);
+        }
+        if (objectProbability > 30 && objectProbability <= 40)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(steelWall, position, Quaternion.identity);
+        }
+        if (objectProbability > 35 && objectProbability <= 40)
+        {
+            Vector3 position = transform.Find(name).position;
+            Instantiate(garbageBags, position, Quaternion.identity);
+        }
+        if (objectProbability > 40 && objectProbability <= 72)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(iceCream, position, Quaternion.identity);
+        }
+        if (objectProbability > 72 && objectProbability <= 74)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(musclePowder, position, Quaternion.identity);
+        }
+        if (objectProbability > 74 && objectProbability <= 100)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(apple, position, Quaternion.identity);
+        }
 
         
- 
     }
     private void SpawnStreetLightAt(string name)
     {
@@ -117,6 +250,23 @@ public class Chunk : MonoBehaviour
         Instantiate(building, position, Quaternion.identity);
     }
 
+    private void calcDifficulty()
+    {
+        GameObject player = GameObject.Find("Player");
+        PlayerRun playerRun = player.GetComponent<PlayerRun>();
+
+        if(playerRun.score >= 10000 && playerRun.score <= 20000)
+        {
+            difficulty = 2;
+            Debug.Log(playerRun.score);
+        }
+
+        if (playerRun.score >= 20000)
+        {
+            difficulty = 3;
+            Debug.Log(playerRun.score);
+        }
+    }
 
 }
 
--- EndlessRunner-Current/New Unity Project/Assets/Scripts/ItemSpawner.cs
+++ EndlessRunner-Current/New Unity Project/Assets/Scripts/ItemSpawner.cs
@@ -8,23 +8,49 @@ public class ItemSpawner : MonoBehaviour
     public GameObject apple;
     public GameObject musclePowder;
     public GameObject iceCream;
+    public int difficulty;
 
     private int item;
     // Start is called before the first frame update
     void Start()
     {
+        calcDifficulty();
+
+        if (difficulty == 1)
+        {
+
             SpawnObjectAt("item1");
-      
+
 
             SpawnObjectAt("item2");
-   
+
 
             SpawnObjectAt("item3");
             SpawnObjectAt("item4");
             SpawnObjectAt("item5");
-        
+        }
 
 
+
+        if (difficulty == 2)
+        {
+
+            SpawnObjectAt2("item1");
+            SpawnObjectAt2("item2");
+            SpawnObjectAt2("item3");
+            SpawnObjectAt2("item4");
+            SpawnObjectAt2("item5");
+        }
+
+        if (difficulty == 3)
+        {
+
+            SpawnObjectAt3("item1");
+            SpawnObjectAt3("item2");
+            SpawnObjectAt3("item3");
+            SpawnObjectAt3("item4");
+            SpawnObjectAt3("item5");
+        }
 
     }
 
@@ -59,4 +85,77 @@ public class ItemSpawner : MonoBehaviour
         }
 
     }
+
+    private void SpawnObjectAt2(string name)
+    {
+        objectProbability = Random.Range(0, 100);
+        
+        if (objectProbability > 1 && objectProbability <= 60)
+        {
+
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(iceCream, position, Quaternion.identity);
+        }
+        
+        if (objectProbability> 99 && objectProbability <= 100)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(musclePowder, position, Quaternion.identity);
+        }
+        
+        if (objectProbability > 60 && objectProbability <= 99)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(apple, position, Quaternion.identity);
+        }
+       
+    }
+    private void SpawnObjectAt3(string name)
+    {
+        
+        objectProbability = Random.Range(0, 100);
+
+        if (objectProbability > 1 && objectProbability <= 60)
+        {
+
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(iceCream, position, Quaternion.identity);
+        }
+        
+        if (objectProbability> 99 && objectProbability <= 100)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(musclePowder, position, Quaternion.identity);
+        }
+        
+        if (objectProbability > 60 && objectProbability <= 99)
+        {
+            Vector3 position = transform.Find(name).position;
+            position.y += .25f;
+            Instantiate(apple, position, Quaternion.identity);
+        }
+        
+    }
+    private void calcDifficulty()
+    {
+        GameObject player = GameObject.Find("Player");
+        PlayerRun playerRun = player.GetComponent<PlayerRun>();
+
+        if (playerRun.score >= 10000&& playerRun.score <= 20000)
+        {
+            difficulty = 2;
+            Debug.Log(playerRun.score);
+        }
+        if (playerRun.score >= 20000)
+        {
+            difficulty = 3;
+            Debug.Log(playerRun.score);
+        }
+    }
+
 }
